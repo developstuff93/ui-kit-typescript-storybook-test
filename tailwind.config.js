8915353a8fa8b565303dffbcce967b789c9362fc
Jose Arias <developstuff93@gmail.com>
2021-08-26 00:19:16 -0400

    add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  purge: ['./src/**/*.{ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      white: "#ffffff",
+      base: "#1b1b1b",
+      blue: {
+        light: "#59679D",
+        dark: "#0076d6",
+      },
+      gray: {
+        light: "#dfe1e2",
+        dark: "#a9aeb1",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
